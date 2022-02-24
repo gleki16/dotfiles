dcccb3de853d48b56797dcf399dd76d1375c6f1d
ray <rraayy246@gmail.com>
2022-02-24 19:56:23 +0800

    space

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,6 @@
-# ~/.config/fish/config.fish
-
 # 如果登入没有图形显示，则启动 sway
 if status is-login ;and test -z "$DISPLAY" -a (tty) = "/dev/tty1"
-		exec sway
-
-# 如果是交互状态，则启动 starship
+	exec sway
 else if status is-interactive
 	starship init fish | source
 	zoxide init fish | source
