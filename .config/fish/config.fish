fc1481449548a82fa075b4b54100a7f9d41049d6
ray <rraayy246@gmail.com>
2022-03-02 23:04:48 +0800

    fish tty1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 # 如果登入没有图形显示，则启动 sway
-if status is-login ;and test -z "$DISPLAY" -a (tty) = "/dev/tty1"
+if test (tty) = "/dev/tty1"
 	exec sway
 else if status is-interactive
 	starship init fish | source
