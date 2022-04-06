d0c57b44910dd8d091b23c01f945401a29adbc57
ray <rraayy246@gmail.com>
2022-04-06 12:30:37 +0800

    zellij

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
 	exec sway
 else if status is-interactive
+	# 如果存在于 ssh 会话中则启动复用器
 	if test -n "$SSH_TTY" -a -z "$ZELLIJ"
 		exec zellij attach --create 0
 	else
