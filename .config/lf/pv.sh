f1af0fd7d6681d3cc89f36b7f8312ae1f766aa08
ray <rraayy246@gmail.com>
2022-04-09 15:52:24 +0800

    previewer

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-bat --color=always --style=plain --pager=never "$1"
+file="$1"
+file_type="$(file -Lb --mime-type -- "$file")"
+
+if test $(du "$file" | awk '{print $1}') -gt 1048576; then
+  echo "file is greater then 1G"
+  exit
+fi
+
+case "$file_type" in
+  text/* | */xml)
+    bat --color=always --style=plain --pager=never "$file"
+    ;;
+  application/zip | application/x-7z-compressed)
+    7z l "$file"
+    ;;
+  *)
+    echo "file type: $file_type, no preview"
+esac
