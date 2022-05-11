079f68b87b08074c73ca2260f73039930ee77b06
ray <rraayy246@gmail.com>
2022-05-11 13:22:23 +0800

    a

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,7 +3,7 @@
 nm-applet &
 dunst &
 fcitx5 &
-feh --bg-fill --no-fehbg --randomize ~/a/pixra/bimple/* &
+feh --bg-fill --no-fehbg --randomize ~/a/pixra/bimple &
 redshift-gtk &
 setxkbmap -option 'ctrl:nocaps' &
 udiskie --smart-tray &
