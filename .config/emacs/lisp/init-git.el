eb6694c8e8972c1a9f0fa89e5a7fe37e5367cb1e
ray <rraayy246@gmail.com>
2022-05-19 23:24:02 +0800

    a

--- .config/emacs/lisp/init-git.el
+++
@@ -1,3 +0,0 @@
-(require 'init-diff-hl)
-
-(provide 'init-git)
--- .config/emacs/lisp/pkg/init-diff-hl.el
+++
@@ -1,7 +0,0 @@
-(use-package diff-hl
-  :hook
-  (dired-mode . diff-hl-dired-mode)
-  (prog-mode . diff-hl-mode)
-  (conf-mode . diff-hl-mode))
-
-(provide 'init-diff-hl)
