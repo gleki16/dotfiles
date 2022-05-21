dcbecd0a29c37111adbc57f82f360ad5875047b9
ray <rraayy246@gmail.com>
2022-05-21 22:20:31 +0800

    go translate

--- .config/emacs/lisp/pkg/init-go-translate.el
+++ .config/emacs/lisp/pkg/init-go-translate.el
@@ -6,18 +6,19 @@
   ;; 翻译语言对
   (gts-translate-list '(("en" "zh")))
 
-  (gts-default-translator
-   (gts-translator
+  :config
+  (setq gts-default-translator
+        (gts-translator
 
-    ;; 用于拾取初始文本
-    :picker (gts-noprompt-picker)
+         ;; 用于拾取初始文本
+         :picker (gts-noprompt-picker)
 
-    ;; 翻译引擎
-    :engines (list
-              (gts-bing-engine)
-              (gts-google-engine))
+         ;; 翻译引擎
+         :engines (list
+                   (gts-bing-engine)
+                   (gts-google-engine))
 
-    ;; 渲染器，用于输出结果到指定目标
-    :render (gts-buffer-render))))
+         ;; 渲染器，用于输出结果到指定目标
+         :render (gts-buffer-render))))
 
 (provide 'init-go-translate)
