bd094e638c1a8c7186302af430e11863ebc074f6
ray <rraayy246@gmail.com>
2022-05-21 12:17:21 +0800

    a

--- .config/emacs/lisp/pkg/init-undo-tree.el
+++ .config/emacs/lisp/pkg/init-undo-tree.el
@@ -1,6 +1,6 @@
 (use-package undo-tree
   :custom
-  (undo-tree-history-directory-alist `(("." . ,(expand-file-name "undo-tree" user-emacs-directory))))
+  (undo-tree-history-directory-alist `(("." . ,(expand-file-name ".undo-tree" user-emacs-directory))))
   (undo-tree-visualizer-diff t)
   (undo-tree-visualizer-timestamps t)
   :config
