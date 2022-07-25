d1b05db6f6c644af378a2304e2bdb4d94b076c8f
ray <rraayy246@gmail.com>
2022-07-25 18:01:14 +0800

    nvim, add q and w

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,1 +1,4 @@
 vim.g.mapleader = ' '           -- 空格键作为 leader 键
+
+vim.api.nvim_set_keymap('n', '<leader>xc', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>xs', ':w<CR>', { noremap = true, silent = true })
