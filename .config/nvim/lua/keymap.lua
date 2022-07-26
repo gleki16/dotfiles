7c13a28291033fee4274b0b3529422e6df0b648f
ray <rraayy246@gmail.com>
2022-07-26 14:38:19 +0800

    keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,18 +1,22 @@
 local keymaps = {}
 
-keymaps['n'] = {
+keymaps[''] = {                 -- 所有模式
+   ['<c-b>'] = '<left>',
+   ['<c-f>'] = '<right>',
+   ['<c-a>'] = '<home>',
+   ['<c-e>'] = '<end>',
+   ['<c-n>'] = '<down>',
+   ['<c-p>'] = '<up>',
+}
+
+keymaps['n'] = {                -- 普通模式
    ['<leader>xc'] = ':q<cr>',
    ['<leader>xs'] = ':w<cr>',
    [';'] = ':',
 }
 
-keymaps['i'] = {
-   ['<c-n>'] = '<down>',
-   ['<c-p>'] = '<up>',
-}
-
 local function load_keymaps()
-   vim.g.mapleader = ' '           -- 空格键作为 leader 键
+   vim.g.mapleader = ' '        -- 空格键作为 leader 键
 
    for mode, mode_map in pairs(keymaps) do
       for key, value in pairs(mode_map) do
