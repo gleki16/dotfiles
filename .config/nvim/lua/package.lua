ee13d0e0dd18cf9ec4ab85179c653e9cd4ee7114
ray <rraayy246@gmail.com>
2022-07-26 16:22:53 +0800

    a

--- .config/nvim/lua/package.lua
+++ .config/nvim/lua/package.lua
@@ -2,17 +2,10 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-   packer_bootstrap = fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
+   fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-require('packer').startup(function(use)
-      -- 有意思的是，packer可以用自己管理自己。
+-- 管理插件
+return require('packer').startup(function(use)
       use 'wbthomason/packer.nvim'
-
-      -- use 'tpope/vim-sensible'
-      -- your plugins here
-
-      if packer_bootstrap then
-         require('packer').sync()
-      end
-                         end)
+                                end)
