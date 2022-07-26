958eda045f6fb7e39505d3b43994f11359cbfd1f
ray <rraayy246@gmail.com>
2022-07-26 13:06:05 +0800

    nvim keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,4 +1,24 @@
-vim.g.mapleader = ' '           -- 空格键作为 leader 键
+local keymaps = {}
 
-vim.api.nvim_set_keymap('n', '<leader>xc', ':q<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>xs', ':w<CR>', { noremap = true, silent = true })
+keymaps['n'] = {
+   ['<leader>xc'] = ':q<cr>',
+   ['<leader>xs'] = ':w<cr>',
+   [';'] = ':',
+}
+
+keymaps['i'] = {
+   ['<c-n>'] = '<down>',
+   ['<c-p>'] = '<up>',
+}
+
+local function load_keymaps()
+   vim.g.mapleader = ' '           -- 空格键作为 leader 键
+
+   for mode, mode_map in pairs(keymaps) do
+      for key, value in pairs(mode_map) do
+         vim.api.nvim_set_keymap(mode, key, value, {noremap = true, silent = true})
+      end
+   end
+end
+
+load_keymaps()
