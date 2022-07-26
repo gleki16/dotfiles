980c0f917c78cda4ede63b8db1fe7d6e91343d63
ray <rraayy246@gmail.com>
2022-07-26 23:29:33 +0800

    a

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -4,23 +4,27 @@ local plugins = {
    require('init-gruvbox'),        -- gruvbox 主题
 }
 
--- 自动安装 packer
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-   packer_bootstrap = fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
-   vim.cmd [[packadd packer.nvim]]
+local function load_plugins()
+   -- 自动安装 packer
+   local fn = vim.fn
+   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+   if fn.empty(fn.glob(install_path)) > 0 then
+      packer_bootstrap = fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
+      vim.cmd [[packadd packer.nvim]]
+   end
+
+   -- 管理插件
+   require('packer').startup(function(use)
+         use 'wbthomason/packer.nvim' -- packer 自身
+
+         for i, plugin in ipairs(plugins) do
+            use(plugin)
+         end
+
+         if packer_bootstrap then
+            require('packer').sync()
+         end
+                            end)
 end
 
--- 管理插件
-require('packer').startup(function(use)
-      use 'wbthomason/packer.nvim' -- packer 自身
-
-      for i, plugin in ipairs(plugins) do
-         use(plugin)
-      end
-
-      if packer_bootstrap then
-         require('packer').sync()
-      end
-                         end)
+load_plugins()
