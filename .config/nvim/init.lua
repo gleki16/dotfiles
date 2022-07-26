1431fe91183b9575ae96ee7d472dacbca7bec23f
ray <rraayy246@gmail.com>
2022-07-26 21:35:35 +0800

    a

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,3 @@
-require('pack')
-require('option')
-require('keymap')
+require('option')               -- 默认选项
+require('keymap')               -- 按键绑定
+require('plugin')               -- 插件设置
---
+++ .config/nvim/lua/plugin.lua
@@ -0,0 +1,26 @@
+local plugins = {
+   require('init-which-key'),      -- 显示按键绑定
+   require('init-nvim-autopairs'), -- 括号自动配对
+   require('init-gruvbox'),        -- gruvbox 主题
+}
+
+-- 自动安装 packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+   packer_bootstrap = fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
+   vim.cmd [[packadd packer.nvim]]
+end
+
+-- 管理插件
+require('packer').startup(function(use)
+      use 'wbthomason/packer.nvim' -- packer 自身
+
+      for i, plugin in ipairs(plugins) do
+         use(plugin)
+      end
+
+      if packer_bootstrap then
+         require('packer').sync()
+      end
+                         end)
