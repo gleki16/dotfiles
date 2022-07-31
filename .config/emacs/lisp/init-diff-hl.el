9324bb282c7a8a3387b191fbafdf404005308325
ray <rraayy246@gmail.com>
2022-08-01 01:00:08 +0800

    diff hl

--- .config/emacs/lisp/init-diff-hl.el
+++ .config/emacs/lisp/init-diff-hl.el
@@ -1,7 +1,8 @@
 (use-package diff-hl
-  :hook (dired-mode . diff-hl-dired-mode)
+  :hook
+  (after-init . global-diff-hl-mode)
+  (dired-mode . diff-hl-dired-mode)
   :config
-  (global-diff-hl-mode)
   (unless (display-graphic-p)
     (diff-hl-margin-mode)))
 
