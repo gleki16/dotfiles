135c3210ec84a75c132f7a6078e42d6c0cf81846
ray <rraayy246@gmail.com>
2022-08-01 09:10:11 +0800

    命令缓冲区注释

--- .config/emacs/lisp/init-vertico.el
+++ .config/emacs/lisp/init-vertico.el
@@ -1,7 +1,10 @@
 (use-package vertico
   :config (vertico-mode))
 
-(use-package savehist
+(use-package savehist                   ; 按历史位置排序
   :config (savehist-mode))
+
+(use-package marginalia                 ; 命令缓冲区注释
+  :config (marginalia-mode))
 
 (provide 'init-vertico)
